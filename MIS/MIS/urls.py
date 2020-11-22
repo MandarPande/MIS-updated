e18b8da4b443e42a0d064faacce5785dae5b73f3
MandarPande <mandarpande29@gmail.com>
2020-11-22 14:30:56 +0530

    Added logout and login functionality for student

--- MIS/MIS/urls.py
+++ MIS/MIS/urls.py
@@ -7,9 +7,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.login),
-    path('home/',views.menu),
+    path('',views.login, name="login"),
+    path('home/',views.menu, name="home"),
     path('achievements/',views.home),
-    path('student_details/',views.details)
+    path('student_details/',views.details),
+    path("logout/", views.logout_request, name="logout")
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- MIS/myapp/views.py
+++ MIS/myapp/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ImageForm
 from .models import Image,Student
 
 # Create your views here.
 def login(request):
+    if request.method == 'POST' :
+        usn = request.POST.get('USN')
+        password = request.POST.get('password')
+        present = False
+        for p in Student.objects.raw("SELECT * FROM myapp_student WHERE USN = '" + usn + "'"):
+            present = True
+        if present and password == 'student@123':
+            return redirect('home')
     return render(request,'login.html')
 
 def menu(request):
@@ -21,3 +29,6 @@ def home(request):
     form = ImageForm()
     img = Image.objects.all()
     return render(request,'home.html',{'img':img,'form':form})
+
+def logout_request(request):
+    return redirect('login')
